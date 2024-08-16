eba45ffa48445eb687d55f76676bbcbcd86c5919
Arjun.s <Arjun.s@ust.com>
2024-08-16 17:44:59 +0530

    Initial Commit

---
+++ src/test/java/com/automation/utils/ConfigReader.java
@@ -0,0 +1,24 @@
+package com.automation.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    static Properties pro;
+
+    public static void initConfig()
+    {
+            pro=new Properties();
+
+            try {
+                pro.load(new FileInputStream("src/test/resources/Config/config.properties"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+    }
+    public static String getConfigValue(String key) {
+        return pro.getProperty(key);
+    }
+
+}
