fbe288e477637fc1b060802a8f796a12a25499f0
Naveen.Sibi <Naveen.Sibi@ust.com>
2024-08-17 12:29:06 +0530

    search commit

--- src/test/java/com/automation/steps/BusHomeSteps.java
+++ src/test/java/com/automation/steps/BusHomeSteps.java
@@ -1,4 +1,33 @@
 package com.automation.steps;
 
+import com.automation.pages.BusHomePage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class BusHomeSteps {
+    BusHomePage busHomePage = new BusHomePage();
+
+    @When("user enter {string} as the source city")
+    public void user_enter_as_the_source_city(String string) {
+        
+        
+    }
+
+    @When("user enter {string} as the destination city")
+    public void user_enter_as_the_destination_city(String string) {
+        
+        
+    }
+
+    @When("user select the date of travel as {string}")
+    public void user_select_the_date_of_travel_as(String string) {
+        
+        
+    }
+
+    @When("user click on the {string} button")
+    public void user_click_on_the_button(String string) {
+        
+        
+    }
 }
