5c57efd8389dfcab63a53203d26322866827c9a2
Naveen.Sibi <Naveen.Sibi@ust.com>
2024-08-31 15:50:48 +0530

    Partially completed openWebsite Function

--- src/test/java/com/automation/pages/mobile/BasePageMobile.java
+++ src/test/java/com/automation/pages/mobile/BasePageMobile.java
@@ -90,6 +90,15 @@ public class BasePageMobile {
                 .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
 
         ((AppiumDriver)driver).perform(Collections.singletonList(sequence));
+//        PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+//        Sequence sequence = new Sequence(finger1, 1)
+//                .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+//                .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+//                .addAction(new Pause(finger1, Duration.ofSeconds(2)))
+//                .addAction(finger1.createPointerMove(Duration.ofSeconds(1), PointerInput.Origin.viewport(), endX, endY))
+//                .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+//
+//        ((AppiumDriver)driver).perform(Collections.singletonList(sequence));
     }
 
 }
