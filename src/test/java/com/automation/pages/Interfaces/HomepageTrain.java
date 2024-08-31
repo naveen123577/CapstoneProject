dc72bf0b99e9dbd6147d5ba3edaa9d62ebf94481
Arjun.s <Arjun.s@ust.com>
2024-08-31 11:00:31 +0530

    Added some interfaces

--- src/test/java/com/automation/pages/Interfaces/HomepageTrain.java
+++ src/test/java/com/automation/pages/Interfaces/HomepageTrain.java
@@ -1,2 +1,36 @@
-package com.automation.pages.Interfaces;public interface HomepageTrain {
+package com.automation.pages.Interfaces;
+
+import org.openqa.selenium.WebDriver;
+
+public interface HomepageTrain {
+
+    void clickTrainTicket();
+
+    void enterSourceCity(String from) throws InterruptedException;
+
+    void enterDestinationCity(String to) throws InterruptedException;
+
+    void clickSearchButton();
+
+    void pickADate(String date);
+
+    void selectDate(String date, WebDriver driver);
+
+    boolean verifyResultsForTrain();
+
+    String getPopupMessage() throws InterruptedException;
+
+    boolean verifySwapButton();
+
+    void clickLiveTrainStatus();
+
+    void enterTrainNumber(String trainNo) throws InterruptedException;
+
+    void clickCheckStatus() throws InterruptedException;
+
+    boolean verifyStatusOfTrain();
+
+    String getErrorMessage();
+
+    boolean verifyTrainHomePage();
 }
--- src/test/java/com/automation/pages/Interfaces/SearchResultBus.java
+++ src/test/java/com/automation/pages/Interfaces/SearchResultBus.java
@@ -1,2 +1,22 @@
-package com.automation.pages.Interfaces;public interface SearchResultBus {
+package com.automation.pages.Interfaces;
+
+public interface SearchResultBus {
+
+    boolean isSearchResultFound();
+    void clickOnSleeperCheckBox();
+    void clickOnAcCheckBox();
+    void clickOnNonAcCheckBox();
+    boolean isNonAcBusResultsDisplayed();
+    void clickOnClearAllFilterBtn();
+    boolean isFullListOfBusesAreDisplayed();
+    boolean isClearAllFilterBtnDisplayed();
+    void clickOnBoardingAndDroppingPointsButton();
+    boolean isBoardingAndDroppingPointsDisplayed();
+    void clickOnEmptySeat();
+    void clickOnViewSeat();
+    boolean isBoardingAndDroppingDetailsListPresent();
+    boolean isSeatsAreVisible();
+    void clickOnHideSeatsButton();
+    void storeFirstBusPriceOfListingPage();
+    boolean isPriceOfListingPageAndBoardingDroppingPageSame();
 }
