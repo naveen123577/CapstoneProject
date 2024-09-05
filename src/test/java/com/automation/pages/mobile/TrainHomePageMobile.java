f4a500f325bede32a7201b00c8a2bb5c8cefd866
Arjun.s <Arjun.s@ust.com>
2024-09-05 14:46:51 +0530

    date function started

--- src/test/java/com/automation/pages/mobile/TrainHomePageMobile.java
+++ src/test/java/com/automation/pages/mobile/TrainHomePageMobile.java
@@ -1,6 +1,7 @@
 package com.automation.pages.mobile;
 
 import com.automation.pages.Interfaces.HomepageTrain;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -30,6 +31,12 @@ public class TrainHomePageMobile extends BasePageMobile implements HomepageTrain
 
     @FindBy(xpath ="//android.widget.TextView[@resource-id='in.redbus.android.redrailsDynamicModule:id/recentSearchTitle']")
     WebElement destFirstOption;
+
+    @FindBy(xpath ="//android.widget.ImageView[@resource-id='in.redbus.android.redrailsDynamicModule:id/dateImage']")
+    WebElement dateIcon;
+
+    @FindBy(xpath ="//android.widget.ImageButton[@content-desc='Next month']")
+    WebElement nextBtn;
 
     @Override
     public void clickTrainTicket() {
@@ -61,12 +68,28 @@ public class TrainHomePageMobile extends BasePageMobile implements HomepageTrain
 
     @Override
     public void pickADate(String date) {
-
+        dateIcon.click();
+        selectDate(date,driver);
     }
 
     @Override
     public void selectDate(String date, WebDriver driver) {
+        String monthYear = date.substring(date.indexOf(" ")+1).toUpperCase();
+        String day = date.substring(0,date.indexOf(" "));
 
+        WebElement monthYearElement = driver.findElement(By.id("in.redbus.android:id/calendar_month_year_textview"));
+
+        String lastFourChars = monthYearElement.getText().substring(monthYearElement.getText().length() - 4);
+
+        while(!monthYear.equals(monthYearElement.getText().substring(0,3)+ " " + lastFourChars)){
+            nextBtn.click();
+            monthYearElement = driver.findElement(By.id("in.redbus.android:id/calendar_month_year_textview"));
+            lastFourChars = monthYearElement.getText().substring(monthYearElement.getText().length() - 4);
+        }
+
+        String xpath = "//android.widget.TextView[@resource-id='in.redbus.android:id/cal_text' and @text='%s']";
+        WebElement dayElement = driver.findElement(By.xpath(String.format(xpath,day)));
+        dayElement.click();
     }
 
     @Override
